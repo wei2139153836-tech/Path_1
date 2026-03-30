19d8ba9aa3317d7eb6b84e67453b00829006133e
wei <2139153836@qq.com>
2026-03-30 10:55:06 +0800

    学习打卡

--- Change_world/Change_world/FileName.cpp
+++ Change_world/Change_world/FileName.cpp
@@ -1,7 +1,36 @@
 #include<stdio.h>
+#include<stdbool.h>
 
 int main() {
-	printf("Change world!\n");
+	//关于注释，用ctrl+/可以作为快捷键
+
+	//关于printf
+	printf("change world!\n");
+
+	//关于字符char的用法
+	signed char a;
+	a='a';
+	printf("%c\n",a);
+
+	int b;
+	b=10;
+	printf("%d\n",b);
+
+	float c;
+	c = 3.14;
+	printf("%f\n",c);
+	
+	long double d;
+	d=3.1415926535897932384626433832795;
+	printf("%Lf\n",d);
+
+	bool e=1;
+	if (e) {
+		printf("It is true!\n");
+	}
+	else {
+		printf("It is false!\n");
+	}
 	return 0;
-	//这是作为系统式学习的第一份程序
+	//这是作为系统式学习的草稿程序
 }
